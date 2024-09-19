c4a1ffd43c7548d1722c750e396b98f653987fbb
Farhan Ali <farhan@example.com>
2024-09-19 16:21:59 +0500

    vite config base commented

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web-dev-jobs/', // Add this line
+ // base: '/web-dev-jobs/', 
   server: {
     port: 3000,
     proxy: {
