9a3dc10f33d55f59e46b8f3946e7ff504c377fef
Farhan Ali <farhan@example.com>
2024-09-19 15:27:46 +0500

    Deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web-dev-jobs/', // Add this line
+  base: '/', // Add this line
   server: {
     port: 3000,
     proxy: {
